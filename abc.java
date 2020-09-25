32a9d8f7c63a8ecdcefa5f65bfd7a84ec8190c5b
Ruqia123 <71782708+Ruqia123@users.noreply.github.com>
2020-09-25 11:47:44 +0500

    files uploaded

---
+++ abc.java
@@ -0,0 +1,21 @@
+package irfank;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class abc { 
+	
+	
+	
+		   @Test 
+			
+		   public void testAdd() {
+		      String str = "Junit is working fine";
+		      assertEquals("Junit is working fine",str);
+		    
+   }
+		
+
+
+} 
